9485c468dbe9ab5553277ce59ce6057c42e845dd
hiftea <hiftielabs@gmail.com>
2023-12-30 15:14:50 +0530

    new resources

--- src/commands/slash/Developers/rsources.js
+++ src/commands/slash/Developers/rsources.js
@@ -16,7 +16,7 @@ module.exports = {
     run: async (client, interaction) => {
 
         const embed = new EmbedBuilder()
-        .setDescription('## Here are some useful resources 🪴💻\n[Bottleneck Calculator](https://pc-builds.com/bottleneck-calculator/): Building a computer? Make sure to checkout this website to make sure your pc don\'t have a bottleneck!\n\n[FPS Calculator](https://pc-builds.com/fps-calculator/): Not sure if your pc is good enough to run games? Check this website to check it out in few steps!\n\n[Port Master](https://safing.io/): Are you a windows user and don\'t want microsoft to send your data to some sketchy sites to "enhance" your user experience? Use portmaster to block all the suspecious network requests!')
+        .setDescription('## Here are some useful resources 🪴💻\n[Bottleneck Calculator](https://pc-builds.com/bottleneck-calculator/): Building a computer? Make sure to checkout this website to make sure your pc don\'t have a bottleneck!\n\n[FPS Calculator](https://pc-builds.com/fps-calculator/): Not sure if your pc is good enough to run games? Check this website to check it out in few steps!\n\n[Port Master](https://safing.io/): Are you a windows user and don\'t want microsoft to send your data to some sketchy sites to "enhance" your user experience? Use portmaster to block all the suspecious network requests!\n\n[Display Specs](https://www.displayspecifications.com): Want to check display specifications of a monitor? Use this website to check all the specs by just entering the monitor\'s model number!')
         .setColor('5e6a74')
 
         if (interaction.user.id === "853147823066578946" ) {
